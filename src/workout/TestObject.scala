52fca947afcb263608d779a9cc122c86270796cf
beakybird <rosan.developer@gmail.com>
2019-06-13 14:17:47 +0530

    Initial scala workout

---
+++ src/workout/TestObject.scala
@@ -0,0 +1,22 @@
+package workout
+
+/**
+  * Test Object for the scala.
+  */
+object TestObject {
+  /**
+    * Main function
+    * @param args
+    */
+  def main(args: Array[String]): Unit = {
+    printMe()
+  }
+
+  /**
+    * Sample function to print
+    */
+ def printMe(): Unit ={
+   println("scala Object")
+ }
+
+}
